c1d6206ea8002d612ae597fe3d53edb22a361a4f
Amit Mainali <amitmainali4@gmail.com>
2025-06-10 09:26:08 -0400

    Implement prepared statement

---
+++ NorthwindTraders2/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,41 @@
+package com.pluralsight;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        if (args.length != 2) {
+            System.out.println("Set <username> and <password>");
+            System.exit(1);
+        }
+
+        String username = args[0];
+        String password = args[1];
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/northwind", username, password);
+
+        String query = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM Products";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        System.out.printf("%-5s %-25s %-10s %-10s\n", "Id", "Name", "Price", "Stock");
+        System.out.printf("----- ------------------------- ---------- ----------\n");
+
+        while (resultSet.next()) {
+            int id = resultSet.getInt("ProductID");
+            String name = resultSet.getString("ProductName");
+            double price = resultSet.getDouble("UnitPrice");
+            int stock = resultSet.getInt("UnitsInStock");
+
+            System.out.printf("%-5d %-25s $%-9.2f %-10d\n", id, name, price, stock);
+        }
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+    }
+}
